672a9328c206925e6d3ee153615ed9790e8276db
Zeroami <826589183@qq.com>
2018-01-29 09:12:38 +0800

    update

--- app/src/main/java/com/zeroami/app/MainModel.kt
+++ app/src/main/java/com/zeroami/app/MainModel.kt
@@ -4,19 +4,14 @@ import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
-import okhttp3.MediaType
 import java.net.URL
-import okhttp3.MultipartBody
-import okhttp3.RequestBody
-import org.json.JSONArray
-import java.io.File
 
 
 class MainModel : IMainModel {
 
     override fun login(account: String, password: String, callback: (e: Throwable?, result: String?) -> Unit): Disposable {
         return Observable.create<String> {
-            it.onNext(URL("http://www.baidu.com").readText())
+            it.onNext(URL("https://www.baidu.com").readText())
         }
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
